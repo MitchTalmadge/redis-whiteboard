b48377146b611a1b2e051047d103ddf699b17cbc
Mitch <mitch@mitchtalmadge.com>
2022-02-06 05:47:06 +0000

    Add basic client

---
+++ client/webpack.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const SRC_DIR = path.join(__dirname, 'src');
+const DIST_DIR = path.join(__dirname, 'dist');
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'entry.tsx'),
+  output: {
+    path: DIST_DIR,
+    filename: 'app.js'
+  },
+  watchOptions: {
+    poll: 500,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(SRC_DIR, 'index.html.ejs'),
+      filename: 'index.html',
+    })
+  ]
+}
+
+export default config;
